2b923f65d30f1faa0ee488ab4ac3484656f4e102
Pratiwi06 <deliapratiwi140@gmail.com>
2025-07-14 05:51:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("Chemical Analysis")
 st.title("_chemical_ is :blue[cool] :sunglasses:")
 import streamlit as st
 
-tab1, tab2, tab3, tab4 = st.tabs(["Cat", "Dog", "Owl, "Tabel Periodik"])
+tab1, tab2, tab3, tab4 = st.tabs(["Cat", "Dog", "Owl", "Tabel"])
 
 with tab1:
     st.header("Regresi Linear ")
