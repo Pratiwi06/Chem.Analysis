7404a1b9ffbd16ec79e046f3927556734fcb70a4
Pratiwi06 <deliapratiwi140@gmail.com>
2025-07-20 07:34:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -422,8 +422,7 @@ with tab5:
 
   
 
-# ==================== TAB 6 =====================
-with tab6:
+# ==================== TAB 6 =====================with tab6:
     st.header("🧪 Standardisasi ")
     st.write("Dalam Normalitas")
     n = st.number_input("Jumlah Ulangan", min_value=2, step=1, value=2)
@@ -436,8 +435,8 @@ with tab6:
 
     for i in range(n):
         st.markdown(f"### Ulangan {i+1}")
-        mg = st.number_input(f"mg Standar Baku Primer - Ulangan {i+1}", key=f"mg_{i}")
-        mL = st.number_input(f"mL Titran - Ulangan {i+1}", key=f"ml_{i}")
+        mg = st.number_input(f"Standar baku primer (mg) - Ulangan {i+1}", key=f"mg_{i}")
+        mL = st.number_input(f"Titran (mL) - Ulangan {i+1}", key=f"ml_{i}")
         BE = st.number_input(f"BE Standar Baku Primer - Ulangan {i+1}", key=f"be_{i}")
         f = st.number_input(f"Faktor pengali - Ulangan {i+1} (jika tidak ada FP, input nilai 1)", key=f"f_{i}")
 
@@ -468,3 +467,4 @@ with tab6:
         st.write(f"Rata-rata Normalitas: **{mean_N:.4f} N**")
         st.write(f"Standar Deviasi (SD): **{std_N:.4f}**")
         st.write(f"%RSD: **{rsd:.2f}%**")
+
